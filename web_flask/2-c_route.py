224c4a7c767c90dac082ff5ff4dec9214482ea80
TaniaHillock <tania.hillock@gmail.com>
2023-08-15 21:18:47 -0600

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,5 +17,11 @@ def hbnb():
     """ Function called with /hbnb route """
     return 'HBNB'
 
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Function called with /c/<text> route """
+    return 'C %s' % text.replace('_', ' ')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
